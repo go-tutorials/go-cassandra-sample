458d56fb7c11ce1405459670e0a91685d3bcb276
Duc Nguyen <minhduc140583@gmail.com>
2024-07-21 18:53:38 +0700

    Upgrade version

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,16 +1,16 @@
 package app
 
 import (
-	"github.com/core-go/core"
+	"github.com/core-go/core/server"
 	mid "github.com/core-go/log/middleware"
 	"github.com/core-go/log/zap"
 )
 
 type Config struct {
-	Server     core.ServerConf `mapstructure:"server"`
-	Cql        Cassandra       `mapstructure:"cassandra"`
-	Log        log.Config      `mapstructure:"log"`
-	MiddleWare mid.LogConfig   `mapstructure:"middleware"`
+	Server     server.ServerConf `mapstructure:"server"`
+	Cql        Cassandra         `mapstructure:"cassandra"`
+	Log        log.Config        `mapstructure:"log"`
+	MiddleWare mid.LogConfig     `mapstructure:"middleware"`
 }
 
 type Cassandra struct {
